931d83ceb2ff560a50c5e38ba7ccdd43daf16102
Kannadasan Poomalai <38586030+kannadasanp@users.noreply.github.com>
2023-01-28 21:52:50 +0530

    Jenkins Changes

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+### STAGE 1: Build ###
+FROM node:16.18.0-alpine AS build
+WORKDIR /usr/src/app
+COPY package.json package-lock.json ./
+RUN npm install
+RUN npm run-script build-autolib-dev
+### STAGE 2: Run ###
+FROM nginx:1.17.1-alpine
+COPY nginx.conf /etc/nginx/nginx.conf
+COPY --from=build /usr/src/app/dist/helpdesk-agent /usr/share/nginx/html
--- src/environments/environment.autolib-dev.ts
+++ src/environments/environment.autolib-dev.ts
@@ -1,8 +1,7 @@
 export const environment = {
-    production: true,
-    apiUrl: 'http://smallcart.in:9021/helpdesk-api/',
-    contentPath: 'http://smallcart.in:9021/helpdesk-api/download/',
-    title: 'DEV - AutoLib - Aatral',
-    logourl: 'https://lh3.googleusercontent.com/U9wexkgs65Pmrv6YRppPZOGcioZpxU4KPgqO2wbq8vwPLxBPy2OzSEqNuMs6OR7XNZ0'
-  };
-  
+  production: true,
+  apiUrl: '/helpdesk-api/',
+  contentPath: '/helpdesk-api/download/',
+  title: 'DEV - AutoLib - Aatral',
+  logourl: 'https://lh3.googleusercontent.com/U9wexkgs65Pmrv6YRppPZOGcioZpxU4KPgqO2wbq8vwPLxBPy2OzSEqNuMs6OR7XNZ0'
+};
